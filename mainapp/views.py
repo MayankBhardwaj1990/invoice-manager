0eae634fe86c163573a2ae379c9d5697574d51ce
mayank <mayankbhardwaj285@gmail.com>
2023-09-25 17:24:29 +0530

    deleted unused code snippets

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,8 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from django.shortcuts import get_object_or_404
-from uuid import UUID
 from . serializers import *
 from . models import *
 
